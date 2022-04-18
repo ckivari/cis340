46f05007581ba39fd9779bba5e9dfa74ee919b99
ckivari <cjkivari@gmail.com>
2022-04-18 03:29:46 +0000

    My Second Project Week 13

---
+++ MySecondProject/App copy 2.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
+
+export default function MyApp() {
+
+  function getFullName(fName, mName, lName){
+    return fName + " " + mName + " " + lName;
+  }
+
+  const pet = "Dog";
+  return (
+    
+      <Text>
+        {"\n\n\n\n\n"}
+        Hello, I am a student in CIS340!{"\n"}
+        My full name is {getFullName("Christian","John","Kivari")}{"\n"}
+        I have a {pet}!
+        </Text>
+      
+  );
+}
