e7295ead085da82db318e95d29f9d262b2027735
ckivari <cjkivari@gmail.com>
2022-05-09 05:06:36 +0000

    Weather Forecast

--- MyEighthProject/App.js
+++ MyEighthProject/App.js
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';// import image picker
+import * as Sharing from 'expo-sharing'; //import image sharing
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+
+    let permissionResult = await ImagePicker.requestCameraRollPermissionAsync();
+
+    if (permissionResult.granted === false){
+      alert("Permission is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if (pickerResult.cancelled === true){
+      return;
+    }
+
+
+    setSelectedImage({localUri: pickerResult.uri})
+  };
+
+  //add the sharing image dialog
+  let openSharingDialogAsync = async () =>  {
+    if (!(await Sharing.isAvailableAsync()))  {
+      alert('Sharing is not available on my phone');
+      return;
+    }
+
+    Sharing.shareAsync(selectedImage.localUri);
+  }
+//display selected image
+  if (selectedImage !== null) {
+    return(
+      <View style = {styles.container}>
+          <Image source = {{uri: selectedImage.localUri}} style = {styles.selImage}/>
+          <TouchableOpacity onPres = {openSharingDialogAsync} style = {styles.button}>
+              <Text style = {styles.buttonText}>Share My Photo</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  }
+
+  // end of the code
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{uri: 'https://raw.githubusercontent.com/AbdunabiRamadan/CIS340/master/images/logo.png'}}
+             style ={styles.logo}/>
+
+      <Text style={styles.insts}>
+        Press the button below to select an image on your phone!
+      </Text>
+      <TouchableOpacity 
+        style = {styles.button}
+        onPress = {openImagePickerAsync}>
+        <Text style = {styles.buttonText}>Pick Image</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +69,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#FFFFE0"
   },
+
+  button: {
+    alignItems: "center",
+    backgroundColor: "#778899",
+    padding: 20,
+    borderRadius: 5,
+  },
+
+ 
+
+  insts:  {
+    fontSize: 18,
+    color: "#87CEFA",
+    marginHorizonal: 15,
+    marginBottom: 10,
+  },
+
+  logo: {
+    width: 310,
+    height: 300,
+    marginBottom: 20
+  },
+
+  buttonText: {
+    fontSize: 20,
+    color: "#fff"
+  },
+
+  selImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain'
+  }
 });
